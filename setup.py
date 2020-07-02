881d354877aec5d4a6857bc475730c131a0f9d05
Alexander Korzun <sahhash33@gmail.com>
2020-07-02 21:53:33 +0300

    Bump version number to 1.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                = 'svec',
-    version             = '1.0.0',
+    version             = '1.1.0',
     description         = 'A simple 2D vector manipulation library for Python, type annotations included',
     author              = 'Alexander Korzun',
     author_email        = 'sahhash33@gmail.com',
