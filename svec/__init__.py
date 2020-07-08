bf505f1e1591bec6a88760b7fdfb24fd348790da
Alexander Korzun <sahhash33@gmail.com>
2020-07-08 20:32:38 +0300

    Add package-level docstring

--- svec/__init__.py
+++ svec/__init__.py
@@ -1,3 +1,10 @@
+"""A simple 2D vector manipulation library
+
+Classes:
+Point2d -- represents a point (coordinates) on a 2D plane
+Vector2d -- represents a vector (change of coordinates) on a 2D plane
+"""
+
 from svec.point2d import Point2d
 from svec.vector2d import Vector2d
 
