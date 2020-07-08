5b51645451541acab6594e824949c576469daceb
Alexander Korzun <sahhash33@gmail.com>
2020-07-08 21:03:11 +0300

    Add docstrings for svec.vector2d

--- svec/vector2d.py
+++ svec/vector2d.py
@@ -1,3 +1,5 @@
+"""Module dedicated to 2D vectors"""
+
 from svec.common import ScalarType
 
 from math import sqrt
@@ -5,31 +7,44 @@ from typing import Tuple
 
 
 class Vector2d:
+    """2D vector"""
     __slots__ = ('x', 'y')
 
     def __init__(self, x: ScalarType, y: ScalarType) -> None:
+        """Construct a vector given its coordinates."""
         self.x = x
         self.y = y
 
     def __add__(self, other: 'Vector2d') -> 'Vector2d':
+        """Add two vectors."""
         return Vector2d(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other: 'Vector2d') -> 'Vector2d':
+        """Subtract `other` from `self`."""
         return Vector2d(self.x - other.x, self.y - other.y)
 
     def __mul__(self, scalar: ScalarType) -> 'Vector2d':
+        """Multiply this vector by a scalar and return the result."""
         return Vector2d(self.x * scalar, self.y * scalar)
 
     def __rmul__(self, scalar: ScalarType) -> 'Vector2d':
         return self * scalar
 
     def __truediv__(self, scalar: ScalarType) -> 'Vector2d':
+        """Divide this vector by a non-zero scalar and return the result.
+
+        Exceptions:
+            ZeroDivisionError -- raised by the floating point division operation
+                                 if `scalar` is zero.
+        """
         return self * (1.0 / scalar)
 
     def __neg__(self) -> 'Vector2d':
+        """Return the additive inverse of `self`."""
         return self * (-1)
 
     def coords(self) -> Tuple[ScalarType, ScalarType]:
+        """Return the coordinates of this vector as a pair of scalars."""
         return self.x, self.y
 
     def __repr__(self) -> str:
@@ -43,15 +58,34 @@ class Vector2d:
         return hash(('vector2d', self.coords())) + 395
 
     def dot(self, other: 'Vector2d') -> float:
+        """Compute the dot product of `self` and `other`.
+
+        If `self == Vector2d(x1, y1)` and `other == Vector2d(x2, y2)`,
+        then the dot product of `self` and `other` is `x1 * x2 + y1 * y2`.
+        """
         return self.x * other.x + self.y * other.y
 
     def length_sq(self) -> float:
+        """Return the squared length of this vector."""
         return self.dot(self)
 
     def __abs__(self) -> float:
+        """Return the length (a.k.a. norm) of this vector
+
+        If it is only necessary to compare the lengths of several vectors,
+        consider calling `length_sq` instead. `length_sq` is faster and it
+        might be more precise because it does not involve computing the square
+        root of the length.
+        """
         return sqrt(self.length_sq())
 
     def normalized(self) -> 'Vector2d':
+        """Return the vector with the same direction but having the length 1.
+
+        This vector must have a non-zero length for this operation to be possible.
+
+        Exceptions:
+            ZeroDivisionError -- raised if this vector is the zero vector"""
         length = abs(self)
         if length == 0.0:
             raise ZeroDivisionError('Cannot normalize the zero vector')
